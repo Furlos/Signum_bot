3c6e318e80d0a49f00406bc7f772310523b5b26f
Yury <yura.shamanov2000@gmail.com>
2023-07-24 11:32:41 +0200

    add type hint for collection

--- signum_bot/database/connection.py
+++ signum_bot/database/connection.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
+from pymongo.collection import Collection
+
 
 client = MongoClient('mongodb://localhost:27017/')  # cluster
 db = client.signum_bot  # create database
 
 # create collections
-students = db['students']
-teachers = db['teachers']
-admins = db['admins']
+students: Collection = db['students']
+teachers: Collection = db['teachers']
+admins: Collection = db['admins']
