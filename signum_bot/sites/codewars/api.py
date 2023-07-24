adfcf53d20a2350475c6791f78af80981dc0e3ee
Yury <yura.shamanov2000@gmail.com>
2023-07-24 11:16:58 +0200

    moved the user_url function to a file urls.py and added exception handling when receiving a user

--- signum_bot/sites/codewars/api.py
+++ signum_bot/sites/codewars/api.py
@@ -1,31 +1,13 @@
-"""
-400 	Bad Request -- Something went wrong
-401 	Unauthorized -- Your API key is wrong
-403 	Forbidden -- You do not have permission to access this resource
-404 	Not Found -- The specified resource could not be found
-405 	Method Not Allowed -- You tried to access a resource with an invalid method
-406 	Not Acceptable -- You requested a format that isn't json
-422 	Unprocessable Entity -- Your input failed validation.
-429 	Too Many Requests -- You're making too many API requests.
-500 	Internal Server Error -- We had a problem with our server. Try again later.
-503 	Service Unavailable -- We're temporarily offline for maintenance. Please try again later.
-"""
-
 import requests
 
-from signum_bot.api.codewars.urls import *
-from signum_bot.api.codewars.user import CodeWarsUser
+from signum_bot.sites.codewars.urls import *
+from signum_bot.sites.codewars import CodeWarsUser
+from sites.codewars.exceptions import check_response_status_code
 
 
-def user_url(username: str) -> str:
-    return GET_USER.replace('{user}', username)
-
-
-def get_user(username: str) -> CodeWarsUser:
+def get_codewars_user(username: str) -> CodeWarsUser:
     response = requests.get(user_url(username))
-    if response.status_code >= 400:
-        # TODO: подумать о возвращаемых данных (думаем вызывать ошибку)
-        return CodeWarsUser()
+    check_response_status_code(response.status_code)
     response = response.json()
     return CodeWarsUser(username=response['username'],
                         honor=response['honor'],
--- signum_bot/sites/codewars/urls.py
+++ signum_bot/sites/codewars/urls.py
@@ -1,2 +1,6 @@
 GET_USER = 'https://www.codewars.com/api/v1/users/{user}'
 LIST_COMPLETED_CHALLENGES = 'https://www.codewars.com/api/v1/users/{user}/code-challenges/completed?page={page}'
+
+
+def user_url(username: str) -> str:
+    return GET_USER.replace('{user}', username)
