ab15b546ac3e4e7f83bb24449ff637f223b7b8be
Yury <yura.shamanov2000@gmail.com>
2023-07-29 10:12:29 +0200

    refactored

--- signum_bot/bot/config.py
+++ signum_bot/bot/config.py
@@ -4,12 +4,6 @@ from enum import Enum
 from loguru import logger
 from dotenv import load_dotenv
 
-_dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(_dotenv_path):
-    load_dotenv(_dotenv_path)
-else:
-    raise FileNotFoundError(f'Not found .env file in {os.path.dirname(__file__)}')
-
 
 class LogLevel(Enum):
     CRITICAL = 'CRITICAL'
@@ -19,8 +13,6 @@ class LogLevel(Enum):
     DEBUG = 'DEBUG'
 
 
-TOKEN = os.getenv('TOKEN')
-
 # logger.remove()
 logger.add(os.path.dirname(__file__) + '/logs/logs_{time}.log',
            level=LogLevel.DEBUG.value,
@@ -28,3 +20,12 @@ logger.add(os.path.dirname(__file__) + '/logs/logs_{time}.log',
            format="{time:DD-MM-YYYY at HH:mm:ss} | "
                   "<level>{level:^8}</level> | "
                   "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>")
+
+_dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(_dotenv_path):
+    load_dotenv(_dotenv_path)
+else:
+    logger.error(f'Not found .env file in {os.path.dirname(__file__)}')
+    raise FileNotFoundError(f'Not found .env file in {os.path.dirname(__file__)}')
+
+TOKEN = os.getenv('TOKEN')
