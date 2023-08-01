53d839aa7ba4acac347bd1b3b50f28bfab676cea
Eytes <yury.shamanov2000@bk.ru>
2023-08-01 22:53:04 +0300

    rename exceptions and add logger

--- signum_bot/sites/codewars/exceptions.py
+++ signum_bot/sites/codewars/exceptions.py
@@ -1,58 +1,61 @@
-class Error400(Exception):
+from loguru import logger
+
+
+class BadRequestError(Exception):
     def __init__(self):
         self.message = "Bad Request -- Something went wrong"
         super().__init__(self.message)
 
 
-class Error401(Exception):
+class UnauthorizedError(Exception):
     def __init__(self):
         self.message = "Unauthorized -- Your API key is wrong"
         super().__init__(self.message)
 
 
-class Error403(Exception):
+class ForbiddenError(Exception):
     def __init__(self):
         self.message = " Forbidden -- You do not have permission to access this resource"
         super().__init__(self.message)
 
 
-class Error404(Exception):
+class NotFoundError(Exception):
     def __init__(self):
         self.message = "Not Found -- The specified resource could not be found"
         super().__init__(self.message)
 
 
-class Error405(Exception):
+class MethodNotAllowedError(Exception):
     def __init__(self):
         self.message = "Method Not Allowed -- You tried to access a resource with an invalid method"
         super().__init__(self.message)
 
 
-class Error406(Exception):
+class NotAcceptableError(Exception):
     def __init__(self):
         self.message = "Not Acceptable -- You requested a format that isn't json"
         super().__init__(self.message)
 
 
-class Error422(Exception):
+class UnprocessableEntityError(Exception):
     def __init__(self):
         self.message = "Unprocessable Entity -- Your input failed validation."
         super().__init__(self.message)
 
 
-class Error429(Exception):
+class TooManyRequestsError(Exception):
     def __init__(self):
         self.message = "Too Many Requests -- You're making too many API requests."
         super().__init__(self.message)
 
 
-class Error500(Exception):
+class InternalServerError(Exception):
     def __init__(self):
         self.message = "Internal Server Error -- We had a problem with our server. Try again later."
         super().__init__(self.message)
 
 
-class Error503(Exception):
+class ServiceUnavailableError(Exception):
     def __init__(self):
         self.message = "Service Unavailable -- We're temporarily offline for maintenance. Please try again later."
         super().__init__(self.message)
@@ -66,22 +69,32 @@ def check_response_status_code(status_code: int) -> None:
 
     match status_code:
         case 400:
-            raise Error400
+            logger.error(f'CodeWars response -> status {status_code} (BadRequestError)')
+            raise BadRequestError
         case 401:
-            raise Error401
+            logger.error(f'CodeWars response -> status {status_code} (UnauthorizedError)')
+            raise UnauthorizedError
         case 403:
-            raise Error403
+            logger.error(f'CodeWars response -> status {status_code} (ForbiddenError)')
+            raise ForbiddenError
         case 404:
-            raise Error404
+            logger.error(f'CodeWars response -> status {status_code} (NotFoundError)')
+            raise NotFoundError
         case 405:
-            raise Error405
+            logger.error(f'CodeWars response -> status {status_code} (MethodNotAllowedError)')
+            raise MethodNotAllowedError
         case 406:
-            raise Error406
+            logger.error(f'CodeWars response -> status {status_code} (NotAcceptableError)')
+            raise NotAcceptableError
         case 422:
-            raise Error422
+            logger.error(f'CodeWars response -> status {status_code} (UnprocessableEntityError)')
+            raise UnprocessableEntityError
         case 429:
-            raise Error429
+            logger.error(f'CodeWars response -> status {status_code} (TooManyRequestsError)')
+            raise TooManyRequestsError
         case 500:
-            raise Error500
+            logger.error(f'CodeWars response -> status {status_code} (InternalServerError)')
+            raise InternalServerError
         case 503:
-            raise Error503
+            logger.error(f'CodeWars response -> status {status_code} (ServiceUnavailableError)')
+            raise ServiceUnavailableError
