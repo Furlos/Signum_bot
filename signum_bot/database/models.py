56dd47b680c03df5b94239b11545f6db18200257
Eytes <yury.shamanov2000@bk.ru>
2023-08-01 22:58:46 +0300

    prescribe the business logic of creating a user, getting statistics on the codewars website

--- signum_bot/database/models.py
+++ signum_bot/database/models.py
@@ -1,6 +1,11 @@
 from typing import Dict, List, Optional
+import dataclasses
 
 from pydantic import BaseModel
+from loguru import logger
+
+import signum_bot.sites.codewars.api as codewars_api
+from signum_bot.database.collections import users, teams, requests
 
 
 class _BaseUserModel(BaseModel):
@@ -30,31 +35,75 @@ class Admin(_BaseUserModel):
         pass
 
 
-class _UserModel(_BaseUserModel):
+class UserModel(_BaseUserModel):
+    role: str
     nicknames: Dict[str, str]  # имена пользователя с разных ресурсов
     teams: List[str]
     own_teams: List[Optional[int]]
 
-    def get_info_about_me(self):
-        pass
+    @staticmethod
+    def get(tg_id: int):
+        """ Получить данные пользователя из БД """
+
+        logger.info(f'Получение пользователя {tg_id} из БД')
+        return UserModel(**users.find_one({'_id': tg_id}, {'_id': 0}))
+
+    @staticmethod
+    def get_nicknames(tg_id: int) -> Dict[str, str]:
+        logger.info(f'Получение всех nicknames пользователя {tg_id} из БД')
+        return UserModel.get(tg_id).nicknames
+
+    def get_codewars_stat(self) -> Dict:
+        """ отправка запроса на сайт codewars для получения данных о пользователе """
+
+        logger.info(f'Попытка получения статистики пользователя {self._id} с сайта codewars')
+        nicknames = UserModel.get_nicknames(self._id)
+        if 'codewars' in nicknames:
+            user_info = codewars_api.get_codewars_user(nicknames.get('codewars'))
+            logger.info(f'Успешное получение статистики пользователя {self._id} с сайта codewars')
+            return dataclasses.asdict(user_info)
+        logger.info(f'Не удалось получить статистику пользователя {self._id} так как он не указал аккаунт codewars')
+        return dict()
 
     def send_request(self):
         """ Отправка запроса на присоединение в команду """
         pass
 
-    def get_info_of_team(self):
+    def get_teams(self):
         pass
 
     def create_team(self):
         pass
 
+    def _create(self, role):
+        logger.info(f'Создание нового {role} -> {self._id}')
+        self.nicknames = {key.lower(): value for key, value in self.nicknames.items()}
+        users.insert_one(
+            {
+                '_id': self._id,
+                'last_name': self.last_name,
+                'name': self.name,
+                'father_name': self.father_name,
+                'role': role,
+                'nicknames': self.nicknames,
+                'teams': [f'{role}s'],
+                'own_teams': []
+            }
+        )
 
-class Student(_UserModel):
+
+class Student(UserModel):
     teacher_id: Optional[int]  # telegram_id преподавателя
 
+    def create(self):
+        self._create('student')
 
-class Teacher(_UserModel):
+
+class Teacher(UserModel):
     students_id: List[Optional[int]]  # telegram_id студентов, привязанных к этому преподавателю
+
+    def create(self):
+        self._create('teacher')
 
     def get_info_of_student(self):
         """ Получение информации о студенте, который прикреплен к данному преподавателю """
@@ -65,7 +114,7 @@ class Teacher(_UserModel):
         pass
 
 
-class TeamLeader(_UserModel):
+class TeamLeader(UserModel):
     def accept_request(self):
         """ Принять запрос на добавлние в команду """
         pass
