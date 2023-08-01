8017d1419e6fd4a102da79514a8f0e8364a2dce7
Eytes <yury.shamanov2000@bk.ru>
2023-08-01 22:57:14 +0300

    create 3 controllers: create_teacher, create_student, codewars_statistic

--- signum_bot/database/controllers.py
+++ signum_bot/database/controllers.py
@@ -1,6 +1,43 @@
-from signum_bot.database.collections import *
+import dataclasses
+from typing import Any
+
+from loguru import logger
+
 from signum_bot.database.models import *
 
 
+def codewars_statistic(tg_id: int) -> Dict:
+    user = UserModel.get(tg_id)
+    return user.get_codewars_stat()
 
 
+def create_student(student_data: dict[str, Any]):
+    """
+    student_data:
+        '_id': tg_id,\n
+        'name': ...,\n
+        'last_name': ...,\n
+        'father_name': ...,\n
+        'teacher_id': ...,\n
+        'nicknames': {
+            'codewars': ...,\n
+            'leetcode': ...\n
+        }
+    """
+    Student(**student_data).create()
+
+
+def create_teacher(teacher_data: dict[str, Any]):
+    """
+    teacher_data:
+        '_id': tg_id,\n
+        'name': ...,\n
+        'last_name': ...,\n
+        'father_name': ...,\n
+        'teacher_id': ...,\n
+        'nicknames': {
+            'codewars': ...,\n
+            'leetcode': ...\n
+        }
+    """
+    Teacher(**teacher_data).create()
