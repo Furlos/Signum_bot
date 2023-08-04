6d514b646e0303fb43e310f07777c09e66d3c331
Eytes <yury.shamanov2000@bk.ru>
2023-08-05 00:33:04 +0300

    fixed a problem when creating a user, his id field could not be called "_id"

--- signum_bot/database/models.py
+++ signum_bot/database/models.py
@@ -1,16 +1,17 @@
 from typing import Dict, List, Optional
 import dataclasses
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from loguru import logger
 
 import signum_bot.sites.codewars.api as codewars_api
 from signum_bot.database.collections import users, teams, requests
+from signum_bot.database.exceptions import *
 
 
 class _BaseUserModel(BaseModel):
-    _id: int  # telegram_id
-    last_name: Optional[str]
+    id: int = Field(alias='_id') # telegram_id
+    last_name: Optional[str] = ''
     name: str
     father_name: str
 
@@ -36,17 +37,19 @@ class Admin(_BaseUserModel):
 
 
 class UserModel(_BaseUserModel):
-    role: str
     nicknames: Dict[str, str]  # имена пользователя с разных ресурсов
-    teams: List[str]
-    own_teams: List[Optional[int]]
+    teams: Optional[List[str]] = []
+    own_teams: Optional[List[int]] = []
 
     @staticmethod
     def get(tg_id: int):
         """ Получить данные пользователя из БД """
 
         logger.info(f'Получение пользователя {tg_id} из БД')
-        return UserModel(**users.find_one({'_id': tg_id}, {'_id': 0}))
+        user = users.find_one({'_id': tg_id})
+        if user:
+            return UserModel(**user)
+        raise NotFoundUserError
 
     @staticmethod
     def get_nicknames(tg_id: int) -> Dict[str, str]:
@@ -56,13 +59,12 @@ class UserModel(_BaseUserModel):
     def get_codewars_stat(self) -> Dict:
         """ отправка запроса на сайт codewars для получения данных о пользователе """
 
-        logger.info(f'Попытка получения статистики пользователя {self._id} с сайта codewars')
-        nicknames = UserModel.get_nicknames(self._id)
-        if 'codewars' in nicknames:
-            user_info = codewars_api.get_codewars_user(nicknames.get('codewars'))
-            logger.info(f'Успешное получение статистики пользователя {self._id} с сайта codewars')
+        logger.info(f'Попытка получения статистики пользователя {self.id} с сайта codewars')
+        if 'codewars' in self.nicknames:
+            user_info = codewars_api.get_codewars_user(self.nicknames.get('codewars'))
+            logger.info(f'Успешное получение статистики пользователя {self.id} с сайта codewars')
             return dataclasses.asdict(user_info)
-        logger.info(f'Не удалось получить статистику пользователя {self._id} так как он не указал аккаунт codewars')
+        logger.info(f'Не удалось получить статистику пользователя {self.id} так как он не указал аккаунт codewars')
         return dict()
 
     def send_request(self):
@@ -75,40 +77,44 @@ class UserModel(_BaseUserModel):
     def create_team(self):
         pass
 
-    def _create(self, role) -> bool:
-        logger.info(f'Попытка создать нового {role} -> {self._id}')
-        if users.find_one({'_id': self._id}):
-            logger.info(f'Пользователь {self._id} уже сущеуствует')
-            return False
-        logger.info(f'Создание нового {role} -> {self._id}')
+    def _create(self, role):
+        logger.info(f'Попытка создать нового {role} -> {self.id}')
+        if users.find_one({'_id': self.id}):
+            logger.info(f'Пользователь {self.id} уже сущеуствует')
+            raise UserExistsError
+        logger.info(f'Создание нового {role} -> {self.id}')
         self.nicknames = {key.lower(): value for key, value in self.nicknames.items()}
+        additional_field = 'teacher_id' if role == 'student' else 'students_id'
         users.insert_one(
             {
-                '_id': self._id,
+                '_id': self.id,
                 'last_name': self.last_name,
                 'name': self.name,
                 'father_name': self.father_name,
                 'role': role,
                 'nicknames': self.nicknames,
                 'teams': [f'{role}s'],
-                'own_teams': []
+                'own_teams': [],
+                additional_field: []
             }
         )
-        return True
 
 
 class Student(UserModel):
-    teacher_id: Optional[int]  # telegram_id преподавателя
+    role: str = 'student'
+    teacher_id: Optional[int] = None  # telegram_id преподавателя
 
-    def create(self) -> bool:
-        return self._create('student')
+    def create(self):
+        print(self.__dict__)
+        self._create(self.role)
 
 
 class Teacher(UserModel):
+    role: str = 'teacher'
     students_id: List[Optional[int]]  # telegram_id студентов, привязанных к этому преподавателю
 
-    def create(self) -> bool:
-        return self._create('teacher')
+    def create(self):
+        self._create(self.role)
 
     def get_info_of_student(self):
         """ Получение информации о студенте, который прикреплен к данному преподавателю """
