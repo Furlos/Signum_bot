3cb54c5631c7079ddbae0c5071b51b05b902764f
Eytes <yury.shamanov2000@bk.ru>
2023-08-13 13:36:35 +0300

    delete TODO

--- signum_bot/sites/user_info.py
+++ signum_bot/sites/user_info.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class CodeWarsUserInfo(BaseModel):
-    # TODO: посмотреть динамическое/автоматичесоке создание полей класса из объекта json
     username: str
     honor: int
     languages: dict
