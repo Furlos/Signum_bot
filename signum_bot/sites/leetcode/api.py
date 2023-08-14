26fdf593d4aa744e4b829e67c85f043c0b2c4626
Eytes <yury.shamanov2000@bk.ru>
2023-08-14 13:42:26 +0300

    add api

--- signum_bot/sites/leetcode/api.py
+++ signum_bot/sites/leetcode/api.py
@@ -3,7 +3,19 @@ from typing import Dict
 import requests
 
 from signum_bot.sites.leetcode.urls import get_user_url
+from signum_bot.sites.leetcode.exceptions import ServerError, UserNotFoundError
 
 
 def get_leetcode_user(nickname: str) -> Dict:
-    return requests.get(get_user_url(nickname)).json()
+    #TODO: изучить работу через cookies
+    response = requests.get(get_user_url(nickname))
+
+    if response.status_code != 200:
+        raise ServerError
+
+    response = response.json()
+
+    if response['status'] == 'error':
+        raise UserNotFoundError(response['message'])
+
+    return response
